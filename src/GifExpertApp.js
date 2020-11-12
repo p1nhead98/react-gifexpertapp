10df5569f4a56e6d061b2ac6638ff4eb57614054
Vicente Carrasco <deathbyhanging1998@gmail.com>
2020-11-12 11:55:13 -0300

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+  
+   
+  const [categories, setCategories] = useState(['Fat Yoshi']);
+
+
+
+
+  return (
+    <>
+
+      <h2>soy algo</h2>
+      <AddCategory setCategories={setCategories}/>
+      <hr></hr>
+
+      
+
+      <ol>
+        {
+          categories.map( category => (
+             <GifGrid 
+              key={category}
+              category={category}
+               />
+          ))
+        }
+      </ol>
+
+    </>
+  )
+
+}
+
+
+
+export default GifExpertApp
